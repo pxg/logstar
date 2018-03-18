2a57cdaadff72fb00e69df5122a9910d5ec09da8
Pete Graham <pete.graham@zerodeposit.com>
2018-03-18 12:56:28 +0000

    Printing main request and response details

--- monkey_patch_requests.py
+++ monkey_patch_requests.py
@@ -1,20 +1,52 @@
 import requests
-from pprint import pprint
 
 
 old_boring_get = requests.get
 
 
-def get_and_log(*args, **kwargs):
+# TODO: requests .get
+def post_and_log(*args, **kwargs):
     """
+    Save request and response data and call original requests get
     """
+    log_request(*args, **kwargs)
     response = old_boring_get(*args, **kwargs)
-    print('log here')
+    log_response(response)
     return response
 
 
-requests.get = get_and_log
+# NOTE: when I add test seperate getting the values from writing the values to
+# the database
+# TODO: add method as a parameter
+def log_request(*args, **kwargs):
+    """
+    Log the details of the requests to Postgres
+    """
+    print('Request url: {}'.format(args[0]))  # could use r.url instead?
+    print('Request method: POST')
+    # Does theses work if they aren't set? Add test for this
+    print('Request headers: {}'.format(kwargs['headers']))
+    print('Request data: {}'.format(kwargs['data']))
 
 
-response = requests.get('http://127.0.0.1:8000/user-agent')
-pprint(response)
+def log_response(response):
+    """
+    Log the details of the response to Postgres
+    """
+    print('Response status code: {}'.format(response.status_code))
+    # Could try and get JSON here
+    print('Response data: {}'.format(response.text))
+    print('Response time: {}'.format(response.elapsed.total_seconds()))
+    print('Response headers: {}'.format(response.headers))
+
+
+requests.post = post_and_log
+
+
+# TODO: run with name arguments to requests
+# TODO: add unit tests
+response = requests.post(
+    'http://127.0.0.1:8000/user-agent?name=pete',
+    data={'key': 'value'},
+    headers={'user-agent': 'my-app/0.0.1'})
+# pprint(response)
--- todo.md
+++ todo.md
@@ -1,10 +1,18 @@
-0. Script making an API request with requests
-1. Configure custom logger so log messages can be captures in code (write to file)
-2. Work out if we can get the response data, headers and status codes
-3. Work out if we can get the URL, query string
-4. Check this works ok for https (using https httbin if available)
-5. Save data to postgres. Set-up flask app with SQLAlchemy and Alembic
+# Immediate
+
+0. Get the response data, headers and status codes
+1. Get the URL, query string
+2. Check this works ok for https (using https httbin if available)
+3. Save data to postgres. Set-up flask app with SQLAlchemy and Alembic
+
+# Later
 
 Script making an API request without requests
 Script making an API request using underlying library
 Script making an API request using async calls
+
+# Tech
+
+Add pytest
+Add CircleCI
+Add black formatter
