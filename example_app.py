99c4294860af82f0a41bf2d5f4d2c937ec2a0601
Pete Graham <pete.graham@zerodeposit.com>
2018-03-25 16:25:36 +0100

    Use base templates for UI app

--- example_app.py
+++ example_app.py
@@ -12,11 +12,10 @@ from weather import Weather
 logstar_on()
 
 # API call using requests
-# TODO: add random value here
-url = 'http://127.0.0.1:8000/user-agent?name={}'.format(randint(0, 1000))
-requests.get(url, data={'key': 'value'})
+# url = 'http://127.0.0.1:8000/user-agent?name={}'.format(randint(0, 1000))
+# requests.get(url, data={'key': 'value'})
 
 # API call using external lib
-# weather = Weather()
-# lookup = weather.lookup(560743)
-# condition = lookup.condition()
+weather = Weather()
+lookup = weather.lookup(560743)
+condition = lookup.condition()
